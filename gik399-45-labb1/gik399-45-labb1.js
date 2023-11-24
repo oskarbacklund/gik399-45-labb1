c2f44fbea0a19f795f92718a06c25a7aafb69e7e
oskarbacklund <112632987+oskarbacklund@users.noreply.github.com>
2023-11-24 11:21:53 +0100

    Add files via upload

---
+++ gik399-45-labb1/gik399-45-labb1.js
@@ -0,0 +1,8 @@
+(<input type="checkbox"></input>)
+    var myElement = document.getElementById("divStyle");
+(<input type="text"></input>)
+    var myElements = document.getElementsByClassName("textfield");
+(<button></button>)
+    var myElements = document.getElementsByClassName("button");
+(<div></div>)
+    var myElements = document.getElementsByClassName("output");
