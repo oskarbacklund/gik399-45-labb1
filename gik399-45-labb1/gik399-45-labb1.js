e1107008d344724edffe577fc10a9a8bccfc789a
oskarbacklund <112632987+oskarbacklund@users.noreply.github.com>
2023-12-01 11:19:52 +0100

    Add files via upload

--- gik399-45-labb1/gik399-45-labb1.js
+++ gik399-45-labb1/gik399-45-labb1.js
@@ -1,8 +1,25 @@
-(<input type="checkbox"></input>)
-    var myElement = document.getElementById("divStyle");
-(<input type="text"></input>)
-    var myElements = document.getElementsByClassName("textfield");
-(<button></button>)
-    var myElements = document.getElementsByClassName("button");
-(<div></div>)
-    var myElements = document.getElementsByClassName("output");
+var checkboxElement = document.getElementById("divStyle")
+
+var textElements = document.getElementsByClassName("textfield")
+
+var buttonElement = document.getElementsByTagName("button") [0]
+
+var outputElement = document.getElementsByClassName("output") [0]
+
+checkboxElement.addEventListener("change", () => {
+    outputElement.style.backgroundColor = textElements [0].value
+})
+
+buttonElement.addEventListener("click", () => {
+    outputElement.remove()
+}
+)
+
+function onInput (event) {
+    console.log("Input target", event.target)
+
+    const targetName = event.target.name
+    if(targetName === "content"){
+        outputElement.innerHTML = textElements[1].value
+    }
+}
